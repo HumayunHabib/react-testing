e2476b46939a8da56d2d72653577f71035165696
HumayunHabib <humayun.habib@magnusmage.com>
2022-10-12 14:34:16 +0500

    form interactions tested

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpPage = () => {
+  const [disabled, setdisabled] = useState(true);
+  const [password, setpassword] = useState("");
+  const [passwordRepeat, setpasswordRepeat] = useState("");
+  // let disabled = false;
+  // setTimeout(() => {
+  //   setdisabled(true);
+  //   // console.log("updating disabled");
+  //   // disabled = true;
+  // }, 1000);
+  const onChangePassword = (event) => {
+    const currentValue = event.target.value;
+    setpassword(currentValue);
+    setdisabled(currentValue != passwordRepeat);
+  };
+  const onChangePasswordRepeat = (event) => {
+    const currentValue = event.target.value;
+    setpasswordRepeat(currentValue);
+    setdisabled(currentValue != password);
+  };
   return (
     <div>
       <h1>Sign Up</h1>
@@ -8,6 +27,17 @@ const SignUpPage = () => {
       <input id="username" />
       <label htmlFor="email">E-mail</label>
       <input id="email" />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" onChange={onChangePassword} />
+      <label htmlFor="passwordRepeat">Password Repeat</label>
+      <input
+        id="passwordRepeat"
+        type="password"
+        onChange={onChangePasswordRepeat}
+      />
+      <button disabled={disabled || password === "" || passwordRepeat === ""}>
+        Sign Up
+      </button>
     </div>
   );
 };
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,11 +1,13 @@
 import SignUpPage from "./SignUpPage";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 describe("Sign UP Page", () => {
   describe("Layout", () => {
     it("has header", () => {
       render(<SignUpPage />);
-      const header = screen.getByText("Sign Up");
-      expect(header).toBeInTheDocument();
+      const header = screen.getAllByText("Sign Up");
+      expect(header[0]).toBeInTheDocument();
     });
 
     it("has username input", () => {
@@ -17,6 +19,47 @@ describe("Sign UP Page", () => {
       render(<SignUpPage />);
       const input = screen.getByLabelText("E-mail");
       expect(input).toBeInTheDocument();
+    });
+    it("has  password", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has password repeat", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password repeat input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has Sign Up button", () => {
+      render(<SignUpPage />);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
+    });
+    it("disables the button initially", () => {
+      render(<SignUpPage />);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
+    });
+  });
+  describe("interactions", () => {
+    it("enables the button when password and password repeat has same value", () => {
+      render(<SignUpPage />);
+      const passwordInput = screen.getByLabelText("Password");
+      const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+      userEvent.type(passwordInput, "P4ssword");
+      userEvent.type(passwordRepeatInput, "P4ssword");
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeEnabled();
     });
   });
 });
